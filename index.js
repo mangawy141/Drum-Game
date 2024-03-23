5ba120685bff55f1042005841566862fdb4f178b
Ahmed_Agha__ <115808351+mangawy141@users.noreply.github.com>
2024-03-23 17:00:05 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+let btn = document.querySelectorAll(".drum");
+
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", function () {
+    let btnText = btn[i].textContent;
+    switch (btnText) {
+      case "w":
+        let tom1 = new Audio(`tom-1.mp3`);
+        tom1.play();
+        break;
+      case "a":
+        let tom2 = new Audio(`tom-2.mp3`);
+        tom2.play();
+        break;
+      case "s":
+        let tom3 = new Audio(`tom-3.mp3`);
+        tom3.play();
+        break;
+      case "d":
+        let tom4 = new Audio(`tom-4.mp3`);
+        tom4.play();
+        break;
+      case "j":
+        let snare = new Audio(`snare.mp3`);
+        snare.play();
+        break;
+      case "k":
+        let kickBass = new Audio(`kick-bass.mp3`);
+        kickBass.play();
+        break;
+      case "l":
+        let crash = new Audio(`crash.mp3`);
+        crash.play();
+        break;
+      default:
+    }
+  });
+}
