a5f42bc6ce32f30e282f64e04ac213b175c673f6
Ahmed_Agha__ <115808351+mangawy141@users.noreply.github.com>
2024-03-23 18:07:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let btn = document.querySelectorAll(".drum");
 for (let i = 0; i < btn.length; i++) {
   btn[i].addEventListener("click", function () {
     let btnText = btn[i].textContent;
+    makeSound(btn[i].innerHTML);
     switch (btnText) {
       case "w":
         let tom1 = new Audio(`tom-1.mp3`);
@@ -35,4 +36,41 @@ for (let i = 0; i < btn.length; i++) {
       default:
     }
   });
+}
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      let tom1 = new Audio(`tom-1.mp3`);
+      tom1.play();
+      break;
+    case "a":
+      let tom2 = new Audio(`tom-2.mp3`);
+      tom2.play();
+      break;
+    case "s":
+      let tom3 = new Audio(`tom-3.mp3`);
+      tom3.play();
+      break;
+    case "d":
+      let tom4 = new Audio(`tom-4.mp3`);
+      tom4.play();
+      break;
+    case "j":
+      let snare = new Audio(`snare.mp3`);
+      snare.play();
+      break;
+    case "k":
+      let kickBass = new Audio(`kick-bass.mp3`);
+      kickBass.play();
+      break;
+    case "l":
+      let crash = new Audio(`crash.mp3`);
+      crash.play();
+      break;
+    default:
+  }
 }
